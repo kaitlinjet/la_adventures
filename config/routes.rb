69cbb07c446a69d2ed7184ebfc0de746fac39c44
Kaitlin Jetviroj <kaitlin.jet@gmail.com>
2015-04-18 16:51:56 -0700

    Created uploader field on Create Adventure. And created static info.html file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   root 'adventures#index'
   resources :adventures
   # The priority is based upon order of creation: first created -> highest priority.
